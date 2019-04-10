0988bb5c49941e3f32f1e24ebc3605ed2609cc3b
shin1rok <skssikm@gmail.com>
2019-04-11 00:03:30 +0900

    バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 256 }
+  validates :content, presence: true, length: { maximum: 65_535 }
 end
