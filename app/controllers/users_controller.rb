6c402f900506e840591861fdfee483728a079345
shin1rok <skssikm@gmail.com>
2019-04-21 13:01:16 +0900

    ユーザ詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    # TODO エラー画面を作る
+    @user = User.find_by!(url_name: params[:username])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
+  scope '/:username', as: :users do
+    get '/', to: 'users#show'
+  end
+
   scope '/:username', module: 'users', as: :users do
     resources :posts
   end
