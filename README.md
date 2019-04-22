1d35bc84fcf078100403af866e49e25a49f123d3
shin1rok <>
2019-04-22 21:21:21 +0900

    開発環境をDockerにする

--- README.md
+++ README.md
@@ -24,3 +24,20 @@ Things you may want to cover:
 ## 実装方針
 ### PostモデルとDraftの関係
 - Postが実態でDraftはPostの状態である
+
+## Dockerコマンド
+```
+# イメージ作成
+docker-compose build
+
+# DBセットアップ
+docker-compose run --rm web bin/rails db:create
+docker-compose run --rm web bin/rails db:migrate
+
+# 起動、停止
+docker-compose up
+docker-compose down
+
+# Gem追加、更新
+docker-compose run --rm web bundle install
+```
--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,21 @@
 
 ActiveRecord::Schema.define(version: 2019_04_21_045246) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
-    t.integer "status", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "url_name"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -35,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_04_21_045246) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
