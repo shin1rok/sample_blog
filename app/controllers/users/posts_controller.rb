f61797e8f2e76b6ae521d714d643b4d73dea04cc
shin1rok <skssikm@gmail.com>
2019-07-06 12:35:17 +0900

    ページネーション追加

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -3,7 +3,7 @@ class Users::PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).page(params[:page])
   end
 
   def show
